ae26c8686956112aedcb8821f01461e0d84ca5c4
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-23 12:21:08 +0530

    css styles

--- website/my-react-app/src/Button/Button.jsx
+++ website/my-react-app/src/Button/Button.jsx
@@ -1,12 +1,25 @@
-import styles from "./Button.module.css"
+import style from "./Button.module.css"
 function Button(){
+
+    const styles = {
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+
     return(
 
         <div>
             <button className="button">Click me</button>
             <br />
             <br />
-            <button className={styles.button}>Click</button>
+            <button className={style.button}>Click</button>
+            <br />
+            <br />
+            <button style={styles}>Dont Click</button>
         </div>
         
     )
